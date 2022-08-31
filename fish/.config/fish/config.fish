cb70d384c01dd60b89cd545475ae7391f01fe2b9
Sjur <sjursaa@gmail.com>
2022-08-31 13:40:16 +0200

    added some fishy stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,5 @@ alias ls="ls -lhp --color --group-directories-first"
 alias cdu="cd /mnt/c/Users/Sjur"
 alias ccat="highlight -O ansi --force"
 alias see='explorer.exe'
+alias bat='batcat'
+alias cds="cd /mnt/c/Users/Sjur/Cloud/Workspace/"
